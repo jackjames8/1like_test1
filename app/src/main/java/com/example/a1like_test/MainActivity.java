3b2e52876077199ceb83de475906c2a8630ff385
jackjames <369631393@qq.com>
2018-05-29 21:00:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a1like_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1like_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+1////
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
